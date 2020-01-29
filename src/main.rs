25b5e8a8a529bfd1f973af26333a680ae42c7777
Andrew Plaza <aplaza@liquidthink.net>
2020-01-29 16:50:47 +0100

    preliminary prototyping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use globset::Glob;
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+
+}
+
+
+fn get_cargo_configs(ignore_target: bool) -> Vec<PathBuf> {
+    let glob = Glob::new("./**/Cargo.toml").expect("Failed").compile_matcher();
+    let ignore_glob = Glob::new("./target/**/Cargo.toml").expect("Failed").compile_matcher();
+    for entry in WalkDir::new("./")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|f| glob.is_match(f.path()))
+        .filter(|f| ignore_target && !ignore_glob.is_match(f.path()))
+    {
+        println!("{:?}", entry.path().display())
+    }
+
 }
