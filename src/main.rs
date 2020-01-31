86caf54696f3adfb5032b8a00fd778c05aeae64b
Andrew Plaza <aplaza@liquidthink.net>
2020-01-31 15:06:34 +0100

    add function for reading config files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
-use globset::{Glob, GlobMatcher};
-use std::path::{PathBuf, Path};
+use globset::Glob;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
+use toml_edit::{value, Document};
 use walkdir::WalkDir;
 
 fn main() {
-    let confs = get_cargo_configs(false);
+    let files = get_cargo_configs(false);
+    let configs = read_configs(&files);
+    configs.iter().for_each(|(_k, f)| println!("{:?}", f));
 
+    // look for key in each toml file that corresponds to user key and  modify
+    // values according to user input
+}
+
+
+
+/// Read a series of File Paths that correspond to Cargo.toml files
+///
+/// # Panics
+/// panics on file open I/O error, if reading the file fails,
+/// if the Cargo.toml is not valid toml, or if the file path
+/// is not valid UTF-8
+fn read_configs(files: &Vec<PathBuf>) -> HashMap<String, Document> {
+    let mut configs = HashMap::new();
+
+    for c in files.iter() {
+        let mut f = File::open(&c).expect("Could not open cargo.toml");
+        let mut buffer = String::new();
+        f.read_to_string(&mut buffer).expect("Reading file failed");
+        let toml = buffer.parse::<Document>().expect("Invalid doc");
+        configs.insert(
+            c.to_str().expect("String not valid unicode").to_string(),
+            toml,
+        );
+    }
+    configs
 }
 
 /// Gets Cargo.tomls, optionally ignoring the /target/ directory, from a
@@ -19,7 +53,7 @@ fn get_cargo_configs(ignore_target: bool) -> Vec<PathBuf> {
 }
 
 fn filter_cargo_configs<P: AsRef<Path>>(path: P, ignore_target: bool) -> bool {
-     let glob = Glob::new("**/Cargo.toml")
+    let glob = Glob::new("**/Cargo.toml")
         .expect("Failed")
         .compile_matcher();
 
@@ -29,14 +63,13 @@ fn filter_cargo_configs<P: AsRef<Path>>(path: P, ignore_target: bool) -> bool {
 
     match ignore_target {
         true => glob.is_match(&path) && !ignore_target_glob.is_match(&path),
-        false => glob.is_match(&path)
+        false => glob.is_match(&path),
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-use super::*;
+    use super::*;
 
     const PATHS: [&str; 9] = [
         "/home/lain/code/chisa/Cargo.toml",
@@ -47,22 +80,30 @@ use super::*;
         "Cargo.toml",
         "./Cargo.toml",
         "./code/kami/Cargo.toml",
-        "./code/kami/src/lib.rs"
+        "./code/kami/src/lib.rs",
     ];
 
     #[test]
     fn should_get_cargo_configs() {
-        let matches = PATHS.iter()
-             .map(|p| filter_cargo_configs(p, true))
-             .collect::<Vec<bool>>();
-        assert_eq!(matches, vec![true, false, true, false, false, true, true, true, false]);
+        let matches = PATHS
+            .iter()
+            .map(|p| filter_cargo_configs(p, true))
+            .collect::<Vec<bool>>();
+        assert_eq!(
+            matches,
+            vec![true, false, true, false, false, true, true, true, false]
+        );
     }
 
     #[test]
     fn should_include_target() {
-        let matches = PATHS.iter()
-                           .map(|p| filter_cargo_configs(p, false))
-                           .collect::<Vec<bool>>();
-        assert_eq!(matches, vec![true, false, true, true, true, true, true, true, false]);
+        let matches = PATHS
+            .iter()
+            .map(|p| filter_cargo_configs(p, false))
+            .collect::<Vec<bool>>();
+        assert_eq!(
+            matches,
+            vec![true, false, true, true, true, true, true, true, false]
+        );
     }
 }
